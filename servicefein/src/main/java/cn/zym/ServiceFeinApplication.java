af774b278f5c419392a04e9cd9a3122e9921830a
gitee_zym <6564995+gitee_zym@user.noreply.gitee.com>
2020-03-23 00:54:01 +0800

    fein服务消费者

--- servicefein/src/main/java/cn/zym/ServiceFeinApplication.java
+++ servicefein/src/main/java/cn/zym/ServiceFeinApplication.java
@@ -7,7 +7,7 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
-//fein服务消费者，伪http客户端，解决负载均衡问题
+//fein服务消费者，伪http客户端，默认继承了ribbon,解决负载均衡问题
 @SpringBootApplication(scanBasePackages = {"cn.zym"})
 @EnableDiscoveryClient
 @EnableFeignClients
